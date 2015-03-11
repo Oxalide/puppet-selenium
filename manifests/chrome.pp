89c68fc00b12a80fd4e8b33a64894ade876d03fc
Amine Zaghdoudi <amine.zaghdoudi@ekino.com>
2015-03-11 16:46:57 +0100

    add instalation of chrome browser and xvfb nedded for selenium chromedriver

--- manifests/chrome.pp
+++ manifests/chrome.pp
@@ -6,20 +6,35 @@ class selenium::chrome(
   $version = "2.10",
   $target_install = '/usr/local/bin'
 ) {
-  $source_file           = "chromedriver_linux32.zip"
+  $source_file           = "chromedriver_linux64.zip"
   $source_url            = "http://chromedriver.storage.googleapis.com/${version}/${source_file}"
-  $exec_name             = 'chromedriver'
-  $target_dir            = '/opt'
+  $target_dir            = '/opt/chromedriver'
 
-  puppi::project::archive { 'chromedriver':
-    source      => $source_url,
-    deploy_root => $target_dir,
-    auto_deploy => true,
-    enable      => true,
+  file { $target_dir:
+    ensure => 'directory',
+    path   => $target_dir,
   }
 
-  file { 'chromedriver_link':
-    ensure => "${target_dir}/chromedriver",
-    path   => "${target_install}/chromedriver",
+  # Todo chrome browser
+
+  wget::fetch { 'chrome-driver':
+    source      => $source_url,
+    destination => "${target_dir}/${source_file}",
+  }->
+  exec { "unzip -u ${source_file}":
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd     => '/opt/chromedriver',
+  }
+
+#  puppi::project::archive { 'chromedriver':
+#    source      => $source_url,
+#    deploy_root => $target_install,
+#    auto_deploy => true,
+#    enable      => true,
+#  }
+
+  file { "${target_install}/chromedriver":
+    ensure => link,
+    target => "${target_dir}/chromedriver",
   }
 }
